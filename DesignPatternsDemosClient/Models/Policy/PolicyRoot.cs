3d0777fdb0c107eb92e8f4d4aadebadd3ab26d54
RichardCorkery <44591336+RichardCorkery@users.noreply.github.com>
2021-11-10 14:39:31 -0500

    PolicyRootConverter: More processing (#3)

--- DesignPatternsDemosClient/Models/Policy/PolicyRoot.cs
+++ DesignPatternsDemosClient/Models/Policy/PolicyRoot.cs
@@ -2,6 +2,15 @@
 {
     public class PolicyRoot
     {
+        public Guid TransactionId { get; set; }
         public string PolicyNumber { get; set; }
+        public DateTime EffectiveDate { get; set; }
+        public DateTime ExpirationDate { get; set; }
+
+        //ToDo: Revisit DateOnly
+        //public DateOnly EffectiveDate { get; set; }
+        //public DateOnly ExpirationDate { get; set; }
+        
+        public decimal Premium { get; set; }
     }
 }
