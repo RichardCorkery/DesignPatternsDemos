7ffc9d9938654d1f076acf83aad922ea8936134b
Richard Corkery <RichardCorkery@hotmail.com>
2021-11-22 09:42:58 -0500

    Added XML File for testing

---
+++ DesignPatternsDemosClient/Services/Minis/RulesEngines/Rules/AgencyConverter.cs
@@ -0,0 +1,22 @@
+﻿using DesignPatternsDemosClient.Models.Acord;
+using DesignPatternsDemosClient.Models.Policy;
+
+namespace DesignPatternsDemosClient.Services.Minis.RulesEngines.Rules
+{
+    public class AgencyConverter : IPolicyConverterRule
+    {
+        public PolicyRoot Convert(Acord inputPolicy, PolicyRoot policy)
+        {
+            //ToDo: Set up: 
+            //  - Global using statements
+            //  - File-scoped Namespaces
+
+            //To Do: Blazor
+            //  - Add Error Bondries
+            //  - Note these on the ReadMe file?
+            //  - Page Title
+
+            return policy;
+        }
+    }
+}
